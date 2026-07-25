4ab22f93101298a22635333bb7a58b8ff876d86e
KevinKhuu27 <khuu.kevin27@hotmail.com>
2026-07-25 14:37:06 -0400

    Updated adult capybara model to work with Blockbench

--- src/main/java/com/epic/animals/client/model/CapybaraModel.java
+++ src/main/java/com/epic/animals/client/model/CapybaraModel.java
@@ -39,30 +39,34 @@ public class CapybaraModel extends EntityModel<CapybaraRenderState> {
         PartDefinition root = mesh.getRoot();
 
         PartDefinition head = root.addOrReplaceChild("head",
-                CubeListBuilder.create().texOffs(0, 0).addBox(-2.5F, -5.0F, -5.0F, 5.0F, 7.0F, 8.0F),
-                PartPose.offset(0.0F, 13.0F, -7.0F));
+                CubeListBuilder.create().texOffs(0, 22).addBox(-2.5F, -6.0F, -7.0F, 5.0F, 7.0F, 8.0F),
+                PartPose.offset(0.0F, 15.0F, -5.0F));
 
-        CubeListBuilder ear = CubeListBuilder.create()
-                .texOffs(20, 24).addBox(-0.5F, -2.0F, -1.0F, 1.0F, 2.0F, 2.0F);
+        head.addOrReplaceChild("right_ear",
+                CubeListBuilder.create().texOffs(24, 38).addBox(0.0F, -2.0F, -1.0F, 1.0F, 2.0F, 2.0F),
+                PartPose.offsetAndRotation(-3.0F, -4.5F, -0.75F, 0.0F, 0.0F, -0.4363F));
+        head.addOrReplaceChild("left_ear",
+                CubeListBuilder.create().texOffs(38, 22).addBox(0.0F, -2.0F, -1.0F, 1.0F, 2.0F, 2.0F),
+                PartPose.offsetAndRotation(2.0F, -5.0F, -0.75F, 0.0F, 0.0F, 0.4363F));
 
         root.addOrReplaceChild("body",
-                CubeListBuilder.create().texOffs(26, 0).addBox(-4.0F, -7.0F, -4.0F, 8.0F, 14.0F, 8.0F),
-                PartPose.offsetAndRotation(0.0F, 16.0F, 1.0F, 1.45F, 0.0F, 0.0F));
+                CubeListBuilder.create().texOffs(0, 0).addBox(-7.0F, -8.0F, -7.0F, 8.0F, 8.0F, 14.0F),
+                PartPose.offsetAndRotation(3.0F, 21.0F, 0.0F, -0.1222F, 0.0F, 0.0F));
 
-        CubeListBuilder leg = CubeListBuilder.create()
-                .texOffs(0, 16).addBox(-1.5F, 0.0F, -1.5F, 3.0F, 5.0F, 3.0F);
+        root.addOrReplaceChild("left_hind_leg",
+                CubeListBuilder.create().texOffs(26, 22).addBox(-1.5F, 0.0F, -1.5F, 3.0F, 5.0F, 3.0F),
+                PartPose.offset(2.0F, 19.0F, 6.5F));
+        root.addOrReplaceChild("right_hind_leg",
+                CubeListBuilder.create().texOffs(12, 37).addBox(-1.5F, 0.0F, -1.5F, 3.0F, 5.0F, 3.0F),
+                PartPose.offset(-2.0F, 19.0F, 6.5F));
+        root.addOrReplaceChild("left_front_leg",
+                CubeListBuilder.create().texOffs(0, 37).addBox(-1.5F, 0.0F, -1.5F, 3.0F, 5.0F, 3.0F),
+                PartPose.offset(2.0F, 19.0F, -3.5F));
+        root.addOrReplaceChild("right_front_leg",
+                CubeListBuilder.create().texOffs(26, 30).addBox(-1.5F, 0.0F, -1.5F, 3.0F, 5.0F, 3.0F),
+                PartPose.offset(-2.0F, 19.0F, -3.5F));
 
-        head.addOrReplaceChild("right_ear", ear,
-                PartPose.offsetAndRotation(-2.5F, -4.0F, 1.5F, 0.0F, 0.0F, -0.436F));
-        head.addOrReplaceChild("left_ear", ear,
-                PartPose.offsetAndRotation( 2.5F, -4.0F, 1.5F, 0.0F, 0.0F,  0.436F));
-
-        root.addOrReplaceChild("right_hind_leg",  leg, PartPose.offset(-2.0F, 19.0F,  7.0F));
-        root.addOrReplaceChild("left_hind_leg",   leg, PartPose.offset( 2.0F, 19.0F,  7.0F));
-        root.addOrReplaceChild("right_front_leg", leg, PartPose.offset(-2.0F, 19.0F, -3.0F));
-        root.addOrReplaceChild("left_front_leg",  leg, PartPose.offset( 2.0F, 19.0F, -3.0F));
-
-        return LayerDefinition.create(mesh, 64, 32);
+        return LayerDefinition.create(mesh, 48, 48);
     }
 
     @Override
@@ -78,5 +82,4 @@ public class CapybaraModel extends EntityModel<CapybaraRenderState> {
         this.rightFrontLeg.xRot = this.leftHindLeg.xRot;
         this.leftFrontLeg.xRot  = this.rightHindLeg.xRot;
     }
-
 }
